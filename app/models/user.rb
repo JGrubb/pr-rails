7ec8fae5ba9e7d26d10d9b0fdb9f611c1f41900b
John Grubb <therealjohnnygrubb@gmail.com>
2012-10-03 06:54:02 -0400

    adding simple for to user sign up, additional fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,18 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :role_id
+
+  validates :first_name, :last_name, :role_id, :presence => true
+  validate :must_be_valid_role
   
   belongs_to :role
+
+  def must_be_valid_role
+    @roles = Role.all
+    unless @roles.include?(role_id)
+      errors.add :role_id, "must be one of the values from the list"
+    end
+  end
 
 end
