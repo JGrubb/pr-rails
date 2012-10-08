6db50ec71420deac73064d26d176fae9f6cdf9e0
John Grubb <therealjohnnygrubb@gmail.com>
2012-10-08 06:54:23 -0400

    create locations model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121007210701) do
+ActiveRecord::Schema.define(:version => 20121008105328) do
+
+  create_table "locations", :force => true do |t|
+    t.string   "address1"
+    t.string   "address2"
+    t.string   "city"
+    t.string   "state"
+    t.string   "zip"
+    t.string   "phone"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "pages", :force => true do |t|
     t.string    "title"
