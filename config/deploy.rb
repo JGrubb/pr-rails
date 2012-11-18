2f0dd9492b07843e670a02303b6ddf498f56d64f
John Grubb <therealjohnnygrubb@gmail.com>
2012-11-17 17:30:40 -0800

    Capfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,16 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "plantright"
+set :repository,  "git@github.com:JGrubb/pr-rails.git"
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+role :web, "plantright"                          # Your HTTP server, Apache/etc
+role :app, "plantright"                          # This may be the same as your `Web` server
+role :db,  "plantright", :primary => true # This is where Rails migrations will run
+
+default_run_options[:pty] = true
+set :user, "rubyuser"
+ssh_options[:forward_agent] = true
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
