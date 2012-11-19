d5c64ff371586fe54854b0b90a05064518544bd0
John Grubb <therealjohnnygrubb@gmail.com>
2012-11-18 18:51:45 -0800

    Adding deliver method to pass quiz

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -12,6 +12,9 @@ class QuizController < ApplicationController
   	score_info = quiz.grade.map { |x| x.to_i }
     session[:grade] = score_info
   	current_user.update_attribute(:verified, quiz.passed?)
+    if quiz.passed?
+      PassQuiz.pass_quiz_email(current_user).deliver
+    end
     @wrong_answers = []
     score_info.each { |i| @wrong_answers << Quiz::questions[i] }
     session[:answers] = @wrong_answers
